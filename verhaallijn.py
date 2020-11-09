4c176ca2c56c914723734b2b581eeaeb0a391c92
Jordan Ross <32171@ma-web.nl>
2020-11-09 12:05:36 +0100

    verhaallijn

---
+++ verhaallijn.py
@@ -0,0 +1,90 @@
+print("Het is maandagochtend je helpt je moeder in de tuin wanneer Haar buurman vraagt of je hem kan helpen in huis met het inzetten van de keuken? ")
+print(" ja graag of nee ik blijf liever thuis.")
+andwoordt1 = "1"
+andwoordt2 = "2"
+vraag1 = input("CHOOSE YOUR ANSWER: typ 1 of 2 :")
+if (vraag1 == "1"):
+    print("kom binnen dan maak ik wat te drinken voor je, tweede deur van rechts is de keueken, Nadat je de tafel hebt verplaatst en hebt geholpen de keuken in te richten, biedt de man je een drankje aan. Alsjeblieft voor je harde werk! Een lekker groot glas sap. Helpt tegen de erge dorst van het harde werk.")
+
+elif (vraag1 == "2"):
+    print("Je blijft lekker thuis en helpt je moeder met het maken van het eten. Je vond de buurman altijd al een beetje apart dus je durfde daar niet echt naar binnen! Jullie hebben het zwaar thuis vanwege het tekort aan geld en eten, daarom help je je moeder zoveel mogelijk. Je besluit boodschappen te doen de volgende morgen, maar onderweg wordt je aangesproken door een vreemd stel die het hebben over een route naar Europa dat ze vanavond met boten langs het water gaan weg varen. Ze vragen of jij en je familie ook mee gaan het blijken kennissen te zijn van je ouders. ")
+
+else:
+    print("What do you mean?! choose 1 or 2")
+
+print("Vraag2 : ga je mee met de kennisen van je ouders of sla je het aanbod af?")
+andwoord3 = "1"
+andwoord4 = "2"
+vraag2 = input("CHOOSE YOUR ANSWER:")
+if (vraag2 == "1"):
+    print("mensen te wachten. Ze willen allemaal vluchten in één boot die eigenlijk niet bedoelt is voor zoveel mensen. Je stapt in de boot en enkele minuten later vertrekken ze van land! Je bent bang voor wat komen gaat en weet niet goed wat je moet doen. Maar je vader houdt je gerust zegt dat alles goed komt maar zojuist dan zie je lampen verschijnen op het water oppervlak. Het is de immigratie politie die iedereen terug stuurt, dat was 3 uur reis voor niks denk je dan. Maar één man uit je boot opent het vuur naar immigratie politie, er ontstaat paniek en een vuurgevecht op water. Het te krappe bootje valt om je wordt bedolven onder een aantal mensen die achter je aan in het water vallen. Je probeert weg te komen maar je merkt dat de man boven op je niet beweegt, en dan zie je het pas een plas bloed drijft van de man af hij is neergeschoten op de boot. Langzaam verdrink je onder de mensen. Je verliest je bewust zijn. Even later wordt je wakker aan land je ziet je vader voor je staan, hij heeft je uit het water gehaald. ")
+
+elif (vraag2 == "2"):
+    print("Je zegt dat het je geen verstandig plan lijkt en gaat verder boodschappen doen, haalt het eten en keert terug naar huis. Daar ga je naar je kamer muziek luisteren totdat je moeder je roept voor het eten. ")
+
+else:
+    print("i could not understand what you meant?!")
+
+print("Vraag2.5 : drink je het glas sap?")
+print("ja of nee?")
+andwoord5 = "ja"
+andwoord6 = "nee"
+vraag3 = input("CHOOSE YOUR AWNSER:")
+if (vraag3 == "ja"):
+    print("Heerlijk sap dankjewel. Jij bedankt zegt de buurman geniet er van, en zodra hij dit zegt verschijnt er een lach op zijn gezicht. Je hoort je moeder roepen vanuit het raam, Sonita waar ben je? We gaan eten! Je kijkt op je klok, Ow is het al zo laat het is al bijna 7 uur ik moet maar eens gaan. Maar zodra je opstaat worden je benen slap, je zakt er bijna doorheen. Je vraagt de buurman om hulp maar die kijkt alleen op je neer. Ik moet naar huis zeg je, me moeder roept! Ik heb andere plannen voor jouw zegt de buurman. Dat is het laatste wat je hoort voordat het zwart voor je ogen wordt. Enige tijd later wordt je wakker maar je houdt je ogen gesloten, je hoort het geluid van een voortuig en je hoort meerdere stemmen praten. Langzaam kom je steeds meer tot je bewustzijn van je omgeving, je  voelt je steeds benauwder en onder drukking alsof er iets op je ligt. Je zicht wordt ietsjes helder je ziet een vreemde vrouw voor je zitten, je schrikt en wilt rechtop zitten maar dat gaat niet er liggen andere tegen je aan. De vrouw kijkt bang net als de rest om haar heen. Je kijkt rond en ziet dat je in een laadruimte van een truck zit. Je wordt meegenomen maar waarheen? En door wie? Je kijkt de vrouw aan")
+
+elif (vraag3 == "nee"):
+    print("Je bedankt de man voor het aanbod, En je hoort je moeder al roepen voor het eten, Sonita waar ben je? We gaan eten. Je loopt door de deur naar buiten het is al donker buiten, het is al erg laat rond 07:00 uur s ’avonds, Sonita leeft verder een normaal armoedig leven. En wordt uiteindelijk verkocht door haar vader aan de slavenhandel waar ze later sterft. (dit is een Alternatief einde)")
+
+else:
+    print("input is incorrect")
+
+print("vraag4 :de vrouw vraagt: Waar worden we heen gebracht? wat is je andwoord?")
+print("Ik heb geen idee laten we het vragen aan die man! of Geen idee maar hou je stil en kijk of er een uitweg is!")
+print("kies 1 of 2 als andwoord")
+andwoord7 = "1"
+andwoord8 = "2"
+vraag4 = input("CHOOSE YOUR AWNSER:")
+if (vraag4 == "1"):
+    print("Je staat op en loopt voorzichtig naar de militair die aan de rand van de truck zit, je tikt hem aan op zijn schouder. Van schrik slaat hij je terug op de grond van de truck. Hij zegt dat je weer moeten gaan zitten maar je wilt alleen wat vragen, dus je staat op maar de militair voelt zich bedreigd dus hij begint te schreeuwen dat je geen stap verder mag doen. Je stopt maar dan een hobbel op de weg je valt vooruit. En de militair aarzelt niet en schiet je meteen neer. Alternatief einde")
+
+elif (vraag4 == "2"):
+    print("Je kijkt snel om je heen en voelt rond met je handen, maar je voelt niks alleen de benen van iemand anders. Maar dan Je ziet een scheur achter je, in het doek van de truck. Je kijkt even om je heen maar niemand let op je dan besluit je eruit te springen, Je kruipt tussen het gat door en houdt je vast aan het doek je bent er halverwege doorheen maar dan, je handen glijden weg door de regen en je valt met je hoofd eerst op de grond. Je probeert op te staan maar je benen voelen slap aan. Je benen kunnen je niet dragen. Je kijkt de truck nog even na maar dan O Nee hij stopt! Ze hebben het gezien. Je kruipt de bosjes in langzaam uit het zicht van de weg af. Dan zie je het pas je been bloed het laat een spoor achter van bloed. De militairen van de truck komen dichterbij je moet snel handelen. Je ziet een groot palm blad liggen en achter je hoor je stemmen van andere mensen, je probeert ze te vragen om hulp maar ze zien je niet. wat doe je nu?")
+
+else:
+    print("could not understand that line")
+
+print("vraag5 :ren je weg of blijf je staan?")
+print("je probeert weg te rennen en te verstoppen achter één van de containers of je blijft vol angst staan wachten wat er gebeurd?")
+andwoord7 = "1"
+andwoord8 = "2"
+vraag4 = input("CHOOSE YOUR AWNSER: 1 of 2:")
+if (vraag4 == "1"):
+    print("Je besluit weg te rennen op de juiste timing je kijkt naar de wachters die heen en weer lopen, je observeert hun loop pad en wacht op het perfecte moment. De bewaker draait om en onmiddellijk begin je je om te draaien en te rennen zo hard als je kan! Je hart gaat al tekeer en door de kou doen je benen erg pijn maar je moet door zetten! Je bent 15 meter verder totdat je een man hoort roepen naar de andere man in een toren, de wachttoren had je helemaal niet opgemerkt en voor je kan stoppen met rennen hoor je al een harde knal, BAM AAAWHHH een kogel vliegt dwars door je buik heen je schreeuwt het uit van de pijn en valt neer. Dit is het einde je voelt het. Door de kou voelt je bloed aan als warm water stromend in de regen zo over je benen op de grond. Je valt neer op je knieën en langzaam verlies je je bewust zijn. De andere vrouwen worden naar binnen gebracht en niemand kijkt om naar jouw je hoort alleen het gehuil van andere meiden en vrouwen. Totdat het geluid steeds meer verzwakt en je je zicht verliest. Het einde van Sonita! EINDE 1")
+elif (vraag4 == "2"):
+    print("vol angst en trillend op je benen van de kou, nat van de extreme regen die op je neer valt. wachtend op wat komen gaat.Je kijkt om je heen alle vrouwen en meiden worden  één voor één op een rij gezet. Jullie zijn van ons nu!  zegt één van de militairen. Wat je voelt wat je denkt wat je doet maakt niet meer uit! Je volgt onze regels hier zegt de militair, hij lijkt wel de commandant. Hij schreeuwt luid dat we naar binnen moeten, we lopen zei aan zij naar binnen, het voelt net als een gevangenis.Zij aan zij worden we een grote ruimte in gebracht, Dit is jullie nieuwe huis zegt de command: Jullie zijn nu ons eigendom als slaven! Er ontstaat onrust onder de mensen, maar de onderdrukking van de Iraanse militairen sust dat snel, een vrouw naast me vertelt me dat volgens de iranen hebben wij vrouwen geen rechten,rechten. Dus het boeit ze niet wat er gebeurt hier. Een paar dagen verder je zit nog steeds vast in deze gevangenis wachtend om verkocht te worden als seks slaaf. Je bent bang in afwachting totdat die dag komt, je weet niet of je de gekte nog aan kan de constante rellen ontsnappingen plannen. Om de tijd te doden schrijf je je muziek op in een klein boekje die je in je jas had zitten. Je schrijft over de strijd die je hebt in dit leven nu. Je vertelt over de pijn van de martelingen, En het gekrijs van smekende vrouwen die smeken om genaden wanneer ze worden verkocht. En bij teveel tegen werking zelfs vermoord! Je kan er niet meer tegen, Je maakt een ontsnapping ’s plan, miala je cel genoot ziet je tekenen en vraagt wat je bent aan het doen?")
+
+else:
+    print("i can't understand that please choose 1 or 2")
+
+print("vraag5 : wat ben je aan het doen?")
+print("je liegt en zegt dat je muziek schrijft of je vertelt dat je gaat proberen te ontsnappen")
+andwoord9 = "1"
+andwoord10 = "2"
+vraag5 = input("CHOOSE YOUR AWNSER: 1 of 2:")
+if (vraag5 == "1"):
+    print("Ow niks bijzonders gewoon muziek aan het schrijven, om de tijd te doden zeg je. Ik weet niet hoe je het uit houdt in dit shit hole want ik kan er niet meer tegen! Zegt Miala, Ik wordt gek in deze muren, Je denkt bij jezelf hoe gek je zelf al wordt en hoe erg je je ouders mist. Was je nou maar thuis gebleven. Denk je, Maar je gaat zorgen dat je terug komt bij ze. Je vraagt aan miala wat ze kwam doen? Ow ja ik wou een rel gaan starten morgen en of je mee wou doen misschien ontsnappen we wel! Zegt miala. Perfect voor jouw ontsnapping ‘s plan denk je! Dus doe je mee?")
+
+elif(vraag5 == "2"):
+    print("Ik ben aan het werken aan een ontsnappings plan maar hou het stil zeg je, miala beloofd dat ze niks zegt en helpt je met het maken van een plan. De volgende dag in de kantine kan je miala niet vinden, En toen zag je haar praten met 1 van de soldaten. Ze wijst naar jouw. Ze heeft je verraden, je cel genoot je vriendin heeft je verraden voor haar eigen vrijheid. Uiteindelijk worden jullie allebij meegenomen door de Iraanse militairen naar buiten toe, Je verwacht het ergste maar wat is dit ze laten je gaan? het hek gaat open en ze zwaaien jullie nog na ook. Langzaam lopen jij en miala uit het hek. Maar zodra je een voet buiten het hek hebt gezet gaat er een alarm af. ze spelen gewoon een spelletje met jullie. Het alarm gaat af en meteen beginnen de mannen te schieten!!! je ziet miala voor je neer vallen en al snel vliegt er bij jouw ook een kogel door je hoofd. EINDE 2")
+
+print("vraag6 : Doe je mee met de rel?")
+print("Ja sowieso! of nee ik denk dat ik deze oversla en wat rust neem.")
+andwoord11 = "1"
+andwoord12 = "2"
+vraag6 = input("CHOOSE YOUR AWNSER: 1 of 2:")
+if (vraag6 == "1"):
+    print("Ja sowieso ik doe mee met de rel, geweldig zegt miala ik roep de andere bij elkaar voor morgen. Zelf besluit je langs de bewakers te sluipen tijdens de rel via de rechter achter uitgang, je werkt je hele plan uit tot erg laat in de avond. Maar de avond klok is gegaan de bewakers komen checken dus je gaat snel slapen zodat de bewakers niks vermoeden. Feuw dat was op het nippertje denk je. De volgende morgen begint de rel tijdens het eten, één vrouw gooit haar dienblad tegen een bewaker aan en begint te vechten onmiddellijk heerst er chaos. Jij duikt zo snel als je kan onder de tafel en gaat de richting van de deur. Mensen springen over tafels mensen met geweren komen nu binnen stormen, de kogels vliegen je om de oren. Er valt een mes van tafel je neemt hem mee werkt perfect voor je ontsnapping! Denk je. Kruipend over de vloer loop je naar deur, mooi niemand let op je.  Met het mes probeer je de deur open te wrikken en te ontsnappen maar je hoort iemand achter je en ja hoor een bewaker, je probeert hem neer te steken maar hij weert je met gemak af en gooit je op de grond. Met je rug naar hem toe dat is het laatste wat je zag. Voordat de kogel je hersenen aan flarden had geschoten op de muur voor je en het licht uit je ogen verdween! EINDE 2")
+
+elif (vraag6 == "2"):
+    print("Je gaat op deze manier door de dagen heen, Geen dag veranderd voor drie jaar lang. Vast in de gevangenis wachtend om verkocht te worden, vele vrouwen zijn gekomen en weggegaan vele vriendinnen die je had gemaakt zijn vervangen door nieuwe. Dagen komen en gaan je boekje is onderandere vol geschreven met teksten die je waarschijnlijk nooit zou kunnen uitbrengen of überhaupt maken. Vast in gevangenschap. Maar dan komt de duistere dag waar je zolang op hebt gevreesd de tijd heeft de dag alleen maar erger gemaakt in je hoofd. Je schreeuwt het uit van angst zodra je hoort dat iemand je heeft gekocht. Uit doodsangsten probeer je de bewakers te bevechten die je je uit je celblok komen halen, maar als snel wordt je overmeesterd en naar de uitgang begeleid. De man die je heeft gekocht ziet er netjes uit maar je laat je niet gek maken, De mannen geven je over en zeggen ze is nu jouw probleem. Probleem denk je bij jezelf, je bent ontvoerd gemarteld en misbruikt in een gevangenis waar je onschuldig inzit en dan ben ik het probleem? De man zegt dat je nu in goeie handen bent. Maar dat heb je onderandere wel vaker gehoord nu. Maar de man was niet geïnteresseerd in je lichaam zei hij, Hij bracht je mee naar zijn boot en je maakte een lange reis, nog steeds heeft de mysterieuze man niks weggegeven van zijn bedoelingen. Je komt aan bij een grote haven iedereen is zo vriendelijk hier. En toen zei de man welkom in Europa! Hij had haar gekocht om te redden, Hij heeft mijn muziek gehoord. Je begint te huilen nadat je hoort dat je thuis dorp is verwoest en je ouders zijn overleden, Maar je roep voor hulp is gehoord. Je krijgt een baan bij een platen maatschappij. Je bent zo blij, En sinds dien werk je bij hun en maak je je eigen verhaal nummers waarin je vertelt over jouw ervaringen in je thuis land en hoe het is om gebruikt te worden, Je verhalen worden wereld wijd gehoord uiteindelijk ga je vechten voor de rechten van vrouwen. Je doet er alles aan om de wereld weer recht te zetten. Sonita nu 23 jaar leeft nog gelukkig en strijd nog steeds voor de rechten van zichzelf en de vrouwen om haar heen. EINDE4")
